bbaac22f931caa479c01f657a9787dc60d715b12
Shashwat <shashwat9621@gmail.com>
2024-05-10 08:15:56 +0530

    Sixth Commit : Created post entity and added its relationship with category and user

--- src/main/java/com/myproject/blog/entities/User.java
+++ src/main/java/com/myproject/blog/entities/User.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="users")
 @NoArgsConstructor
@@ -19,4 +22,6 @@ public class User {
     private String password;
     private String email;
     private String about;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
 }
