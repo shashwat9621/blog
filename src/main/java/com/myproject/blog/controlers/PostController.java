320b2ecc59067367d4d72796577aef875a1c2419
Shashwat <shashwat9621@gmail.com>
2024-05-26 21:52:57 +0530

    11th Commit :  image upload and serving.........serving might be not working correctly need to check. One more doubt, in the database image name is not attached to uuid

--- src/main/java/com/myproject/blog/controlers/PostController.java
+++ src/main/java/com/myproject/blog/controlers/PostController.java
@@ -1,17 +1,26 @@
 package com.myproject.blog.controlers;
 
+import com.myproject.blog.config.AppConstants;
 import com.myproject.blog.dto.PostDto;
-import com.myproject.blog.dto.UserDto;
 import com.myproject.blog.payloads.ApiResponse;
 import com.myproject.blog.payloads.PostResponse;
+import com.myproject.blog.services.FileService;
 import com.myproject.blog.services.PostService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -19,6 +28,10 @@ import java.util.List;
 public class PostController {
     @Autowired
     private PostService postService;
+    @Autowired
+    private FileService fileService;
+    @Value("${project.image}")
+    private String path;
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId,@PathVariable Integer categoryId){
         PostDto createPost = this.postService.createPost(postDto,userId,categoryId);
@@ -38,10 +51,10 @@ public class PostController {
 
     @GetMapping("/posts")
     public ResponseEntity<PostResponse> getAllPost(
-            @RequestParam(value="pageNumber", defaultValue = "0",required = false) Integer pageNumber,
-            @RequestParam(value="pageSize", defaultValue = "10",required = false) Integer pageSize,
-            @RequestParam(value="sortBy", defaultValue = "postId",required = false) String sortBy,
-            @RequestParam(value="sortDir", defaultValue = "asc",required = false) String sortDir
+            @RequestParam(value="pageNumber", defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+            @RequestParam(value="pageSize", defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize,
+            @RequestParam(value="sortBy", defaultValue = AppConstants.SORT_BY,required = false) String sortBy,
+            @RequestParam(value="sortDir", defaultValue = AppConstants.SORT_DIR,required = false) String sortDir
 
             )
     {
@@ -71,6 +84,30 @@ public class PostController {
         List<PostDto> post =this.postService.searchPosts(keywords);
         return new ResponseEntity<List<PostDto>>(post,HttpStatus.OK);
     }
+
+    @PostMapping("/post/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(
+            @RequestParam("image")MultipartFile image,
+            @PathVariable Integer postId) throws IOException {
+        PostDto postDto= this.postService.getPostById(postId);
+        String fileName = this.fileService.uploadImage(path,image);
+        postDto.setImageName(fileName);
+        PostDto updatePost = this.postService.updatePost(postDto,postId);
+        return  new ResponseEntity<PostDto>(updatePost,HttpStatus.OK);
+
+    }
+    @GetMapping("/post/images/{imageName},produces=MediaType.IMAGE_JPEG_VALUE")
+    public void downloadImage(
+            @PathVariable("imageName") String imageName,
+            HttpServletResponse response
+    ) throws IOException {
+        InputStream resource =this.fileService.getResource(path,imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
+
+    }
+
+
 
 
 }
